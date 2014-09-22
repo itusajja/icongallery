2a92a12f77bc9584adf09dd53b6c82d9213a3921
Jim Nielsen <jimniels@gmail.com>
2014-09-22 14:38:57 -0400

    update sync-new script

--- _build/_sync-new.sh
+++ _build/_sync-new.sh
@@ -3,9 +3,14 @@
 # Get the domain we're working with
 read -p "What domain? [ios/mac] " DOMAIN
 
-# Dry run upload to S3
-echo -e "\n--> Dry Run upload to S3...\n"
-s3cmd sync --dry-run --acl-public --guess-mime-type --skip-existing --exclude-from _files.exclude ../_site/ s3://macicongallery.com/
+# Switch to _site folder
+cd ../_site/
+
+# Dry run new and upload to S3 for new and dependant files
+echo -e "\n--> Dry run sync new files...\n"
+s3cmd sync --dry-run --acl-public --guess-mime-type --skip-existing --exclude-from ../_build/_files.exclude . s3://${DOMAIN}icongallery.com/
+echo -e "\n--> Dry run put dependencies...\n"
+s3cmd put --dry-run --acl-public --guess-mime-type -r index.html data.json feed.xml p s3://${DOMAIN}icongallery.com/
 
 # If dry run is ok, continue sync; otherwise exit
 echo # new line
@@ -14,11 +19,9 @@ echo    # (optional) move to a new line
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
     echo -e "\n--> Syncing new files...\n"
-    s3cmd sync --acl-public --guess-mime-type --skip-existing --exclude-from _files.exclude ../_site/ s3://macicongallery.com/
+    s3cmd sync --acl-public --guess-mime-type --skip-existing --exclude-from ../_build/_files.exclude . s3://${DOMAIN}icongallery.com/
     echo -e "\n--> Putting new file dependencies...\n"
-    s3cmd put --acl-public --guess-mime-type ../_site/index.html s3://macicongallery.com/
-    s3cmd put --acl-public --guess-mime-type ../_site/data.json s3://macicongallery.com/
-    s3cmd put --acl-public --guess-mime-type ../_site/feed.xml s3://macicongallery.com/
+    s3cmd put --acl-public --guess-mime-type -r index.html data.json feed.xml p s3://${DOMAIN}icongallery.com/
     echo -e "\n--> Done!"
 fi
 echo #extra line
