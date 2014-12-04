4b91174c491be70293a940c4c9999c63f33e06ae
Jim Nielsen <jimniels@gmail.com>
2014-12-04 16:27:15 -0500

    JS for handling strings (commented out)

--- assets/scripts/custom.js
+++ assets/scripts/custom.js
@@ -1,3 +1,23 @@
+// check for exisiting
+// var urlParams;
+// (window.onpopstate = function () {
+//     var match,
+//         pl     = /\+/g,  // Regex for replacing addition symbol with a space
+//         search = /([^&=]+)=?([^&]*)/g,
+//         decode = function (s) { return decodeURIComponent(s.replace(pl, " ")); },
+//         query  = window.location.search.substring(1);
+
+//     urlParams = {};
+//     while (match = search.exec(query))
+//        urlParams[decode(match[1])] = decode(match[2]);
+// })();
+
+// console.log(urlParams.key);
+// if(urlParams.key) {
+//     console.log('Load '+urlParams.key);
+// } else {
+//     console.log('Load default active');
+// }
 
 var Filter = {
     key: null,
@@ -161,6 +181,8 @@ $(document).ready(function(){
             Icons.intialize(data);
         }
     });
+
+
 
     // Initialize
     Filter.init();
