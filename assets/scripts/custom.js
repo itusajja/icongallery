760b6e3e601c84430a5a97d4ba6668c1c0ea50c7
Jim Nielsen <jimniels@gmail.com>
2015-08-28 22:18:55 -0600

    initial commit on react way work

--- assets/scripts/custom.js
+++ assets/scripts/custom.js
@@ -1,3 +1,215 @@
+var IconBox = React.createClass({
+
+  propTypes: {
+    icons: React.PropTypes.array.isRequired,
+    threshold: React.PropTypes.number.isRequired
+  },
+
+  getInitialState: function(){
+    return {
+      activeCategory: '',
+      iconCount: this.props.icons.length,
+      //currentIcons: this.props.icons.slice(0, this.props.threshold),
+      visibleIcons: this.props.icons.slice(0, this.props.threshold),
+      hiddenIcons: this.props.icons,
+    };
+  },
+
+  handleUserInput: function(activeCategory) {
+    var icons = [];
+    this.props.icons.forEach(function(icon) {
+        // if (product.name.indexOf(this.props.filterText) === -1 || (!product.stocked && this.props.inStockOnly)) {
+        //     return;
+        // }
+        if (activeCategory !== '') {
+          if(activeCategory == icon.category){
+            icons.push(icon);
+          }
+        } else {
+            icons.push(icon);
+        }        
+    }.bind(this));
+
+    this.setState({
+      activeCategory: activeCategory,
+      iconCount: icons.length,
+      visibleIcons: icons.slice(0, this.props.threshold),
+      hiddenIcons: icons
+    });
+  },
+
+  filterIcons: function(activeCategory) {
+    var icons = [];
+    this.props.icons.forEach(function(icon) {
+        // if (product.name.indexOf(this.props.filterText) === -1 || (!product.stocked && this.props.inStockOnly)) {
+        //     return;
+        // }
+        if (activeCategory !== '') {
+          if(activeCategory == icon.category){
+            icons.push(icon);
+          }
+        } else {
+            icons.push(icon);
+        }        
+    }.bind(this)); 
+    return icons;
+  },
+
+  // getIcons: function(){
+  //   const newIcons = this.props.icons.slice(this.state.currentIcons.length, this.state.currentIcons.length + showMoreThreshold);
+  //   return this.state.currentIcons.concat( newIcons );
+  // },
+
+  handleShowMore: function(e) {
+    e.preventDefault();
+    var newIcons = this.state.hiddenIcons.slice(this.state.visibleIcons.length, this.state.visibleIcons.length + this.props.threshold);
+    this.setState({
+      visibleIcons: this.state.visibleIcons.concat( newIcons )
+    });
+  },
+
+  render: function() {
+    return (
+      <div>
+        <IconFilters 
+          iconCount={this.state.iconCount}
+          activeCategory={this.state.activeCategory}
+          onUserInput={this.handleUserInput}
+        />
+        <IconList icons={this.state.visibleIcons} /> 
+        {(this.state.hiddenIcons.length > this.state.visibleIcons.length) ? 
+          <ShowMoreButton onShowMore={this.handleShowMore}/>
+          : null
+        }       
+      </div>
+    );
+  }
+});
+
+
+
+
+var IconList = React.createClass({
+  propTypes: {
+    icons: React.PropTypes.array.isRequired,
+  },
+
+  render: function() {
+    return (
+        <ul className="list-icons" >
+          {
+            (this.props.icons.length > 1) ? this.props.icons.map(function(icon) {
+              return <IconItem key={icon.id} icon={icon}/>;
+            }) : <IconItemZeroState />
+          } 
+        </ul>
+    );
+  }
+});
+
+var IconItem = React.createClass({
+  
+  render: function() {
+    // Make link
+    var year = new Date(this.props.icon.date).getFullYear();
+    var link = '/' + year + '/' + this.props.icon.slug + '/';
+    var iconFile = this.props.icon.slug + '-' + year;
+
+    return (
+      <li>
+        <a href={link} className="icon-container" title={this.props.icon.title}>
+          <img
+            alt={this.props.icon.title + 'app icon'}
+            className="icon icon-128"
+            src={'/applewatchicongallery/img/128/' + iconFile + '.png'}
+            data-at2x={'/applewatchicongallery/img/256/' + iconFile + '.png'}
+          />
+          {/* mask here */}
+        </a>    
+      </li>
+    );
+  }
+});
+
+
+
+var IconItemZeroState = React.createClass({
+  render: function() {
+    return (
+      <li className="list-icons__zero-state">No icons. Try changing your filters.</li>
+    );
+  }
+});
+
+
+
+
+var ShowMoreButton = React.createClass({
+  propTypes: {
+    onShowMore: React.PropTypes.func.isRequired
+  },
+
+  render: function(){
+    return (
+      <a href="#" className="show-more" onClick={this.props.onShowMore}>
+        Show more&hellip;
+      </a>
+    )
+  },
+});
+
+
+
+
+var IconFilters = React.createClass({
+  propTypes: {
+    iconCount: React.PropTypes.number,
+    activeCategory: React.PropTypes.string,
+    onUserInput: React.PropTypes.func
+  },
+
+  handleChange: function() {
+    this.props.onUserInput(
+        this.refs.categoryInput.getDOMNode().value
+    );
+  },
+
+  render: function() {
+    return (
+      <div className="search-header">
+        <span className="search-header__count">{this.props.iconCount} icons</span>
+        <select id="category" className="filter-val" onChange={this.handleChange} ref="categoryInput"> <option value="">All categories...</option> <option value="6018">Books</option> <option value="6000">Business</option> <option value="6022">Catalogs</option> <option value="6017">Education</option> <option value="6016">Entertainment</option> <option value="6015">Finance</option> <option value="6023">Food &amp; Drink</option> <option value="6014">Games</option> <option value="6013">Health &amp; Fitness</option> <option value="6012">Lifestyle</option> <option value="6020">Medical</option> <option value="6011">Music</option> <option value="6010">Navigation</option> <option value="6009">News</option> <option value="6021">Newsstand</option> <option value="6008">Photo &amp; Video</option> <option value="6007">Productivity</option> <option value="6006">Reference</option> <option value="6005">Social Networking</option> <option value="6004">Sports</option> <option value="6003">Travel</option> <option value="6002">Utilities</option> <option value="6001">Weather</option> </select>
+      </div>
+    );
+  }
+});
+
+
+
+
+// Kick it off by getting the initial data
+var iconData = []
+$(document).ready(function(){
+    $.ajax({
+        url: '/data.json',
+        dataType: 'json',
+        async: false,
+        success: function(data) {
+            iconData = data;
+            React.render(
+              <IconBox icons={iconData} threshold={12} />,
+              document.getElementById('main')
+            );
+        }
+    });
+});
+
+
+
+
+
+
+/*
 var Icons = {
     data: [],
     results: [],
@@ -144,3 +356,4 @@ $(document).ready(function(){
     });
 
 });
+*/
