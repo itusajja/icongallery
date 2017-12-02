f5253d00aa9319a94473947986884cdaec64db0c
Jim Nielsen <jimniels@gmail.com>
2017-12-02 08:44:16 -0700

    Fixes for modal functionality (still disabled)

--- static/scripts/global.js
+++ static/scripts/global.js
@@ -36,117 +36,117 @@ if (img512) {
   img1024.src = src1024;
 }
 
-// /**
-//  * 
-//  * Modal Links
-//  * Only try doing a modal if it's the home page or a "pagination" page
-//  * 
-//  */
+/**
+ * 
+ * Modal Links
+ * Only try doing a modal if it's the home page or a "pagination" page
+ * 
+ */
 
-// if (location.pathname === "/" || location.pathname.substring(0, 3) === "/p/") {
-//   addModalFunctionality();
-// }
+if (location.pathname === "/" || location.pathname.substring(0, 3) === "/p/") {
+  // addModalFunctionality();
+}
 
-// function addModalFunctionality() {
-//   /**
-//    * VARS
-//    */
-//   // We keep track of which image is loading, in case user closes modal
-//   // while the image is still loading we can reset the onload event
-//   var $modalIconCurrentlyLoading = null;
-//   var $modal = createEl(
-//     '<div class="modal">' +
-//       '<button class="modal__close">&#215;</button>' +
-//       '<div class="modal__body"></div>' +
-//     '</div>'
-//   );
-//   var $modalBody = $modal.querySelector('.modal__body');
-//   var $modalLoader = createEl(
-//     '<img src="/shared/img/loading.gif" alt="loading" width="32" height="32" />'
-//   );
-//   var $modalIcon = createEl(
-//     '<img width="512" height="512" class="icon" />'
-//   );
+function addModalFunctionality() {
+  /**
+   * VARS
+   */
+  // We keep track of which image is loading, in case user closes modal
+  // while the image is still loading we can reset the onload event
+  var $modalIconCurrentlyLoading = null;
+  var $modal = createEl(
+    '<div class="modal">' +
+      '<button class="modal__close">&#215;</button>' +
+      '<div class="modal__body"></div>' +
+    '</div>'
+  );
+  var $modalBody = $modal.querySelector('.modal__body');
+  var $modalLoader = createEl(
+    '<img src="/shared/static/img/loading.gif" alt="loading" width="32" height="32" />'
+  );
+  var $modalIcon = createEl(
+    '<img width="512" height="512" class="icon" />'
+  );
 
-//   /**
-//    * Modal
-//    * Add modal to DOM.
-//    * Add listeners so any click outside of `.modal__body` will close the modal
-//    */  
-//   $modal.addEventListener("click", function() {
-//     // Hide the modal
-//     document.body.classList.remove("show-modal");
-//     // If there's an icon that's still loading, ny currently loading icon
-//     if ($modalIconCurrentlyLoading) {
-//       $modalIconCurrentlyLoading = null;
-//     }
-//   });
-//   $modal.querySelector('.modal__body').addEventListener("click", function(e) {
-//     e.stopPropagation();
-//   });
-//   document.body.appendChild($modal);
+  /**
+   * Modal
+   * Add modal to DOM.
+   * Add listeners so any click outside of `.modal__body` will close the modal
+   */  
+  $modal.addEventListener("click", function() {
+    // Hide the modal
+    document.body.classList.remove("show-modal");
+    // If there's an icon that's still loading, ny currently loading icon
+    if ($modalIconCurrentlyLoading) {
+      $modalIconCurrentlyLoading = null;
+    }
+  });
+  $modal.querySelector('.modal__body').addEventListener("click", function(e) {
+    e.stopPropagation();
+  });
+  document.body.appendChild($modal);
 
-//   /**
-//    * Icons
-//    * Declare a render icon function for when the modal icon loads
-//    * Add listener to all the icons on the page
-//    */
-//   var renderIcon = function(iconName, iconLink) {
-//     var $modalBodyHtml = createEl(
-//       '<a href="'+iconLink+'" class="modal-link" title="'+iconName+'">' +
-//         '<span class="icon-wrapper icon-wrapper--512"></span>' +
-//         '<span class="modal-link__title">'+iconName+'</span>' +
-//       '</a>'
-//     );
-//     $modalBodyHtml.querySelector('.icon-wrapper').appendChild($modalIcon);
+  /**
+   * Icons
+   * Declare a render icon function for when the modal icon loads
+   * Add listener to all the icons on the page
+   */
+  var renderIcon = function(iconName, iconLink) {
+    var $modalBodyHtml = createEl(
+      '<a href="'+iconLink+'" class="modal-link" title="'+iconName+'">' +
+        '<span class="icon-wrapper icon-wrapper--512"></span>' +
+        '<span class="modal-link__title">'+iconName+'</span>' +
+      '</a>'
+    );
+    $modalBodyHtml.querySelector('.icon-wrapper').appendChild($modalIcon);
 
-//     $modalBody.innerHTML = '';
-//     $modalBody.appendChild($modalBodyHtml);
+    $modalBody.innerHTML = '';
+    $modalBody.appendChild($modalBodyHtml);
 
-//     $modalIconCurrentlyLoading = null;
-//   }
+    $modalIconCurrentlyLoading = null;
+  }
 
-//   var $icons = [].slice.call(document.querySelectorAll(".js-trigger-icon-modal"));
-//   $icons.forEach(function($icon) {
-//     $icon.addEventListener("click", function(e) {
-//       e.preventDefault();
+  var $icons = [].slice.call(document.querySelectorAll(".js-trigger-icon-modal"));
+  $icons.forEach(function($icon) {
+    $icon.addEventListener("click", function(e) {
+      e.preventDefault();
 
-//       // Get meta info about clicked icon
-//       var iconName = $icon.getAttribute("title");
-//       var iconLink = $icon.getAttribute("href");
+      // Get meta info about clicked icon
+      var iconName = $icon.getAttribute("title");
+      var iconLink = $icon.getAttribute("href");
 
-//       // Show the modal with a loading indicator until everything loads
-//       $modalBody.innerHTML = '';
-//       $modalBody.appendChild($modalLoader);
-//       document.body.classList.add("show-modal");
+      // Show the modal with a loading indicator until everything loads
+      $modalBody.innerHTML = '';
+      $modalBody.appendChild($modalLoader);
+      document.body.classList.add("show-modal");
 
-//       // Listeners for when the icon loads. First try for a 1024 image. If that
-//       // fails, it's because there's no 1024 size, so get a 512 (which should
-//       // be guaranteed)
-//       $modalIcon.onload = function() {
-//         $modalIcon.setAttribute('alt', iconName);
-//         renderIcon(iconName, iconLink);
-//       };
-//       $modalIcon.onerror = function() {
-//         // Only replace if it's 1024. We don't want an infinite loop
-//         if ($modalIcon.src.indexOf("/1024/") !== -1) {
-//           $modalIcon.src = $modalIcon.src.replace("/1024/", "/512/");
-//         }
-//       };
+      // Listeners for when the icon loads. First try for a 1024 image. If that
+      // fails, it's because there's no 1024 size, so get a 512 (which should
+      // be guaranteed)
+      $modalIcon.onload = function() {
+        $modalIcon.setAttribute('alt', iconName);
+        renderIcon(iconName, iconLink);
+      };
+      $modalIcon.onerror = function() {
+        // Only replace if it's 1024. We don't want an infinite loop
+        if ($modalIcon.src.indexOf("/1024/") !== -1) {
+          $modalIcon.src = $modalIcon.src.replace("/1024/", "/512/");
+        }
+      };
       
-//       // Kick it off by trying to load the 1024 version and set our currently
-//       // loading icon in case we have to cancel
-//       $modalIcon.src = $icon
-//         .querySelector("img")
-//         .getAttribute("src")
-//         .replace("/128/", "/1024/");
-//       $modalIconCurrentlyLoading = $modalIcon;
-//     });
-//   });
-// }
+      // Kick it off by trying to load the 1024 version and set our currently
+      // loading icon in case we have to cancel
+      $modalIcon.src = $icon
+        .querySelector("img")
+        .getAttribute("src")
+        .replace("/128/", "/1024/");
+      $modalIconCurrentlyLoading = $modalIcon;
+    });
+  });
+}
 
-// function createEl(str) {
-//   var div = document.createElement("div");
-//   div.innerHTML = str;
-//   return div.firstChild;
-// }
+function createEl(str) {
+  var div = document.createElement("div");
+  div.innerHTML = str;
+  return div.firstChild;
+}
