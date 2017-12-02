7780f68397b1a5953d0d0f2e8070411f88442515
Jim Nielsen <jimniels@gmail.com>
2017-12-02 08:14:35 -0700

    Move API data directly into search page

--- _src/scripts/entry.jsx
+++ _src/scripts/entry.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import getJSON from "./utils/getJSON";
 import SearchIcons from "./components/SearchIcons";
 
-// If it's the search page, load the data & kick off react
-getJSON("/api.json", data => {
-  ReactDOM.render(
-    <SearchIcons icons={data.icons} site={data.site} threshold={20} />,
-    document.getElementById("root")
-  );
-});
+ReactDOM.render(
+  <SearchIcons
+    icons={window.ICON_GALLERY.icons}
+    site={window.ICON_GALLERY.site}
+    threshold={20}
+  />,
+  document.getElementById("root")
+);
