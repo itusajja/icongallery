9c3d0b947136f69945d9929f3a24f903115f60df
Jim Nielsen <jimniels@gmail.com>
2018-12-13 09:13:34 -0700

    initial commit

--- src/www/assets/scripts/global.js
+++ src/www/assets/scripts/global.js
@@ -26,7 +26,7 @@ body[0].addEventListener("click", function(e) {
  */
 
 if (location.pathname === "/" || location.pathname.substring(0, 3) === "/p/") {
-  // addModalFunctionality();
+  addModalFunctionality();
 }
 
 function addModalFunctionality() {
@@ -68,6 +68,14 @@ function addModalFunctionality() {
   });
   document.body.appendChild($modal);
 
+  document.addEventListener("click", e => {
+    const activeEl = document.querySelector(".active");
+    console.log(activeEl);
+    activeEl.style = "";
+    activeEl.classList.remove("active");
+    document.querySelector(".active-icon").classList.remove("active-icon");
+  });
+
   /**
    * Icons
    * Declare a render icon function for when the modal icon loads
@@ -89,43 +97,79 @@ function addModalFunctionality() {
     $modalIconCurrentlyLoading = null;
   };
 
+  var styles = {
+    // transform: scale(4) translate3d(50%, 0%, 0);
+    position: "fixed",
+    zIndex: "10000",
+    transformOrigin: "left"
+  };
+
   var $icons = [].slice.call(
     document.querySelectorAll(".js-trigger-icon-modal")
   );
   $icons.forEach(function($icon) {
     $icon.addEventListener("click", function(e) {
       e.preventDefault();
+      e.stopPropagation();
+
+      $icon.parentNode.classList.add("active-icon");
+      console.log($icon.parentNode);
+
+      const {
+        left,
+        right,
+        bottom,
+        top,
+        x,
+        y
+      } = e.target.getBoundingClientRect();
+      const windowCenterX = window.innerWidth / 2; // 1680/2=840 middle point of screen
+      const windowCenterXOffset = windowCenterX - 256; // 840-256=584;
+      const xTranslate = (windowCenterXOffset - left) / 4; // 4 for our scale
+
+      const windowCenterY = window.innerHeight / 2; // 780/2=470 middle point of screen
+      const windowCenterYOffset = windowCenterY - 256 - window.scrollY; // 840-256=584;
+      const yTranslate = (windowCenterYOffset - top) / 4; // 4 for our scale
+
+      console.log(`scale(4) translate3d(${xTranslate}, ${yTranslate}, 0)`);
+      e.target.classList.add("active");
+      e.target.style.position = "fixed";
+      e.target.style.zIndex = "100000";
+      e.target.style.transformOrigin = "left top 0";
+      e.target.style.transform = `scale(4) translate3d(${xTranslate}px, ${yTranslate}px, 0)`;
 
       // Get meta info about clicked icon
-      var iconName = $icon.getAttribute("title");
+      // var iconName = $icon.getAttribute("title");
       var iconLink = $icon.getAttribute("href");
 
-      // Show the modal with a loading indicator until everything loads
-      $modalBody.innerHTML = "";
-      $modalBody.appendChild($modalLoader);
+      // // Show the modal with a loading indicator until everything loads
+      // $modalBody.innerHTML = "";
+      // $modalBody.appendChild($modalLoader);
       document.body.classList.add("show-modal");
 
       // Listeners for when the icon loads. First try for a 1024 image. If that
       // fails, it's because there's no 1024 size, so get a 512 (which should
       // be guaranteed)
-      $modalIcon.onload = function() {
-        $modalIcon.setAttribute("alt", iconName);
-        renderIcon(iconName, iconLink);
-      };
-      $modalIcon.onerror = function() {
-        // Only replace if it's 1024. We don't want an infinite loop
-        if ($modalIcon.src.indexOf("/1024/") !== -1) {
-          $modalIcon.src = $modalIcon.src.replace("/1024/", "/512/");
-        }
-      };
+      const $img = $icon.querySelector("img");
+      $img.src = $img.src.replace("/128", "/1024");
+      // $modalIcon.onload = function() {
+      //   $modalIcon.setAttribute("alt", iconName);
+      //   renderIcon(iconName, iconLink);
+      // };
+      // $modalIcon.onerror = function() {
+      //   // Only replace if it's 1024. We don't want an infinite loop
+      //   if ($modalIcon.src.indexOf("/1024/") !== -1) {
+      //     $modalIcon.src = $modalIcon.src.replace("/1024/", "/512/");
+      //   }
+      // };
 
       // Kick it off by trying to load the 1024 version and set our currently
       // loading icon in case we have to cancel
-      $modalIcon.src = $icon
-        .querySelector("img")
-        .getAttribute("src")
-        .replace("/128/", "/1024/");
-      $modalIconCurrentlyLoading = $modalIcon;
+      // $modalIcon.src = $icon
+      //   .querySelector("img")
+      //   .getAttribute("src")
+      //   .replace("/128/", "/1024/");
+      // $modalIconCurrentlyLoading = $modalIcon;
     });
   });
 }
